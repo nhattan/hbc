1e4bfcf49b30cfd813d87731b79d23d02ae108f3
Nhat Tan <nhattan2012@gmail.com>
2014-06-17 17:09:30 +0700

    CRUD products with paperclip

---
+++ db/migrate/20140617100410_add_image_columns_to_products.rb
@@ -0,0 +1,9 @@
+class AddImageColumnsToProducts < ActiveRecord::Migration
+  def self.up
+    add_attachment :products, :image
+  end
+
+  def self.down
+    remove_attachment :products, :image
+  end
+end
